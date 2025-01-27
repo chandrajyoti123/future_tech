244ef8e1bc50d876b0a9c929f6f290feec684743
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2025-01-27 15:12:42 +0530

    host changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/future_tech"
+  base:"/future_tech",
+  server: {
+    port: 3000, // Change this to your desired port
+    host: '0.0.0.0', // (Optional) Use this to bind to all network interfaces
+  },
 })
